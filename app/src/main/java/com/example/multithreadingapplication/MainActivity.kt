b1db724b6781add77e8a4f66db09684ee4667fbd
Andrii Krenevych <krenevych@knu.ua>
2024-10-22 09:04:34 +0300

    synchronius loading

--- app/src/main/java/com/example/multithreadingapplication/MainActivity.kt
+++ app/src/main/java/com/example/multithreadingapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.multithreadingapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.multithreadingapplication.databinding.ActivityMainBinding
 
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("SetTextI18n")
     private fun loadData() {
 
         Log.d(TAG, "loadData: start data loading from internet...")
@@ -30,20 +33,43 @@ class MainActivity : AppCompatActivity() {
 
         // on Start:
             // disable button "load data"
+        binding.btnLoadData.isEnabled = false
             // show progress bar
+        binding.progressBar.visibility = View.VISIBLE
+
             // clear City
+        binding.tvCityValue.text = ""
             // clear Temperature
+        binding.tvTemperatureValue.text = ""
             // show Toast that data is started loading.
 
         // on Progress
             // load City
+        val city: String = loadCity()
                // and set it into correspondent text view
+        binding.tvCityValue.text = city
             // then load temperature for loaded City,
+        val temperature: Int = loadTemperature(city)
                // and set it into correspondent text view
+        binding.tvTemperatureValue.text = temperature.toString()
 
         // on Finish:
             // hide progress bar
+        binding.progressBar.visibility = View.GONE
+
             // enable button "load data"
+        binding.btnLoadData.isEnabled = true
+    }
+
+    private fun loadCity(): String {
+        Thread.sleep(3_000)  // simulate loading...
+
+        return "Kyiv"
+    }
+
+    private fun loadTemperature(city: String): Int {
+        Thread.sleep(3_000)  // simulate loading...
+        return 9
     }
 
     companion object {
