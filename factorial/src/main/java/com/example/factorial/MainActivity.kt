0b2015a48d3c88fdf78ed74cff13eb7080f74807
Andrii Krenevych <krenevych@knu.ua>
2024-10-28 12:07:35 +0200

    Base functional for factorial application

--- factorial/src/main/java/com/example/factorial/MainActivity.kt
+++ factorial/src/main/java/com/example/factorial/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.factorial
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.factorial.databinding.ActivityMainBinding
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(binding.root)
+
+        binding.btnCalcFactorial.setOnClickListener {
+            val f = calculateFactorial(binding.etNum.text.toString().toInt())
+            binding.tvFactorial.text = f.toString()
         }
+    }
+
+
+    private fun calculateFactorial(n: Int) : BigInteger {
+        var f = BigInteger.ONE
+        for (i in 1..n){
+            f *= i.toBigInteger()
+        }
+        return f
     }
 }
