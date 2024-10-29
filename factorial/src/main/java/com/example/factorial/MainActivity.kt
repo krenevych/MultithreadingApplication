68a87ca41588776f18f976259e39fa87aa0133bd
Andrii Krenevych <krenevych@knu.ua>
2024-10-29 09:44:13 +0200

    working app

--- factorial/src/main/java/com/example/factorial/MainActivity.kt
+++ factorial/src/main/java/com/example/factorial/MainActivity.kt
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.factorial.databinding.ActivityMainBinding
 import java.math.BigInteger
 
@@ -31,12 +34,36 @@ class MainActivity : AppCompatActivity() {
 //            val viewModel1: MainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 //            viewModel
 //            Log.d("XXX", "onCreate: ")
+            binding.tvFactorial.text = ""
             val n = binding.etNum.text.toString()
             viewModel.calculateFactorial(n)
         }
 
+        binding.btnCancel.setOnClickListener {
+            viewModel.cancelCalculation()
+        }
+
         viewModel.factorialLD.observe(this) { fact: String ->
             binding.tvFactorial.text = fact
+        }
+
+        viewModel.error.observe(this) { err: Boolean ->
+            if (err)
+                Toast.makeText(this, "Please enter valid integer!", Toast.LENGTH_SHORT).show()
+        }
+
+        viewModel.progress.observe(this) { inProgress: Boolean ->
+            if (inProgress) {
+                binding.progressBar.visibility = View.VISIBLE
+                binding.etNum.isEnabled = false
+                binding.btnCalcFactorial.isEnabled = false
+                binding.btnCancel.isEnabled = true
+            } else {
+                binding.progressBar.visibility = View.GONE
+                binding.etNum.isEnabled = true
+                binding.btnCalcFactorial.isEnabled = true
+                binding.btnCancel.isEnabled = false
+            }
         }
 
     }
--- factorial/src/main/java/com/example/factorial/MainViewModel.kt
+++ factorial/src/main/java/com/example/factorial/MainViewModel.kt
@@ -3,6 +3,10 @@ package com.example.factorial
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
@@ -12,13 +16,50 @@ class MainViewModel : ViewModel() {
     val factorialLD : LiveData<String>
         get() = _factorialLD
 
+    private val _error = MutableLiveData<Boolean>()
+    val error : LiveData<Boolean>
+        get() = _error
+
+    private val _progress = MutableLiveData<Boolean>()
+    val progress : LiveData<Boolean>
+        get() = _progress
+
+
+    val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     fun calculateFactorial(n: String?){
-        val n_int = n?.toInt()
-        val res = factorial(n_int!!).toString()
-        _factorialLD.value = res
+        if (n.isNullOrEmpty()) {
+            _error.value = true
+            _progress.value = false
+            return
+        }
+
+        _progress.value = true
+        _error.value = false
+
+        val n_int = n.toInt()
+
+        coroutineScope.launch {
+            val res = factorial(n_int).toString()
+            _factorialLD.postValue(res)  // postValue - setup data in Main thread.
+            _progress.postValue(false)
+//            _factorialLD.value = res
+//            _progress.value = false
+        }
+
     }
 
+
+    override fun onCleared() {
+        super.onCleared()
+
+        coroutineScope.cancel()
+    }
+
+    fun cancelCalculation() {
+        coroutineScope.cancel()
+        _progress.postValue(false)
+    }
 
     private fun factorial(n: Int): BigInteger {
         var f = BigInteger.ONE
